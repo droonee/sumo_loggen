5380b13361d76f076d5a63e56c27596ce13ebcd2
David Rooney <91282834+droonee@users.noreply.github.com>
2022-12-01 14:49:43 -0700

    update code

--- sumo_personal_loggen.py
+++ sumo_personal_loggen.py
@@ -2,34 +2,36 @@
 import json
 import datetime
 import time
-import numpy
 import random
+import requests
 from tzlocal import get_localzone
 
-outputfile = "apache_gen.log"
+apache_url = "<http endpoint>"
 
-def loggen(outputfile):
+def apache_log():
     local_time = get_localzone()
-    local_time = datetime.datetime.now(local_time).strftime('%Y-%m-%d %H:%M:%S')
-    
+    local_time = datetime.datetime.now(
+        local_time).strftime('%Y-%m-%d %H:%M:%S')
+
     inputfile = open('./log_profile.json')
     jsonfile = json.load(inputfile)
-    
-    with open(outputfile, 'a') as outfile:
-        priv_ipv4 = numpy.random.choice(jsonfile["private_ipv4"])
-        pub_ipv4 = numpy.random.choice(jsonfile["public_ipv4"])
-        method = numpy.random.choice(jsonfile["method"],p=[0.3,0.2,0.2,0.1,0.2])
-        filepath = numpy.random.choice(jsonfile["filepath"])
-        status_code = numpy.random.choice(jsonfile["status_code"])
-        bytes = numpy.random.choice(jsonfile["bytes"],p=[0.15,0.15,0.2,0.4,0.1])
-        uri = numpy.random.choice(jsonfile["uri"])
-        useragent = numpy.random.choice(jsonfile["user_agent"])
-        outfile.write('%s %s - - [%s] "%s %s HTTP/1.0" %s %s "%s" "%s"\n' % (priv_ipv4,pub_ipv4,local_time,method,filepath,status_code,bytes,uri,useragent))
-        outfile.flush()
-    
+
+    priv_ipv4 = random.choice(jsonfile["private_ipv4"])
+    pub_ipv4 = random.choice(jsonfile["public_ipv4"])
+    method = random.choice(jsonfile["method"])
+    filepath = random.choice(jsonfile["filepath"])
+    status_code = random.choice(jsonfile["status_code"])
+    bytes = random.choice(jsonfile["bytes"])
+    uri = random.choice(jsonfile["uri"])
+    useragent = random.choice(jsonfile["user_agent"])
+    mydata = ('%s %s - - [%s] "%s %s HTTP/1.0" %s %s "%s" "%s"\n' % (priv_ipv4,
+              pub_ipv4, local_time, method, uri, status_code, bytes, filepath, useragent))
+    req = requests.post(apache_url, data=mydata)
+
+    sleeptime = random.choice(range(3, 20))
+    time.sleep(sleeptime)
+
     inputfile.close()
 
 while True:
-    loggen(outputfile)
-    sleeptime = random.uniform(2,20)
-    time.sleep(1)
+    apache_log()
