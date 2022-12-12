c28b557c408780d79177284fcad473cf46367fe8
David Rooney <91282834+droonee@users.noreply.github.com>
2022-12-11 19:11:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ import sys
 os.system("sudo apt update")
 os.system("sudo apt install python3-pip")
 os.system("pip install -r requirements.txt")
-os.system("echo 'export HTTP_ENDPOINT={args}' >> ~/.bashrc".format(args=sys.argv[1]))
+os.system("export HTTP_ENDPOINT={args}".format(args=sys.argv[1]))
