29a5933528d96f02f3d54fd80eac1f81c5cb6566
David Rooney <91282834+droonee@users.noreply.github.com>
2022-12-14 15:19:29 -0700

    Create sumo_loggen.py

---
+++ sumo_loggen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import datetime
+import random
+import time
+from tzlocal import get_localzone
+from master import *
+
+# add your Sumo HTTP endpoint here
+HTTP_ENDPOINT = ""
+
+while True:
+    local_time = get_localzone()
+    local_time = datetime.datetime.now(local_time).strftime('%Y-%m-%d %H:%M:%S')
+
+    ### list of services to choose from ###
+    # Apache
+    # Tenable
+    # ProofpointTAP
+
+    service = Apache # modify service from the list above to generate that data 
+    service.loggen(HTTP_ENDPOINT, local_time)
+
+    sleep = random.choice(range(3,20))
+    time.sleep(sleep)
